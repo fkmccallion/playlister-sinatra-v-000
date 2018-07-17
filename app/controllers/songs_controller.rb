c137692d0bb2108ec3dd34cfbb308333902cf998
Frank McCallion <fkmccallion@gmail.com>
2018-07-17 12:00:20 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,6 +39,13 @@ class SongsController < ApplicationController
     erb :'songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
+
+    erb:'songs/edit'
+  end
+
   post '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
 
@@ -50,13 +57,4 @@ class SongsController < ApplicationController
 
     erb :'songs/show'
   end
-
-  get '/songs/:slug/edit' do
-    @song = Song.find_by_slug(params[:slug])
-    @genres = Genre.all
-
-    erb:'songs/edit'
-  end
-
-
 end
